cde408eb267a53dfaf792fce29297c3924323d4e
Sushant <sdahal249@gmail.com>
2024-04-20 04:46:10 -0400

    graphs completed

--- fidelity_hackathon/server/index.js
+++ fidelity_hackathon/server/index.js
@@ -6,11 +6,11 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
-// import productRoutes from "./routes/product.js";
-// import transactionRoutes from "./routes/transaction.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
 import KPI from "./models/KPI.js";
-// import Product from "./models/Product.js";
-// import Transaction from "./models/Transaction.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
 import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS */
@@ -26,8 +26,8 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
-// app.use("/product", productRoutes);
-// app.use("/transaction", transactionRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 console.log("hello")
 
@@ -44,8 +44,8 @@ mongoose
 //     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
-//     // Product.insertMany(products);
-//     // Transaction.insertMany(transactions);
+    // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
  
